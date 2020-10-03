f79ebed050f8a9ee54e33099820a9220493e08ec
Duongvanquang <15114129@st.hcmuaf.edu.vn>
2020-10-03 18:56:50 +0700

    funciton

---
+++ Desktop/Javascipt/buoi1.js
@@ -0,0 +1,27 @@
+// khai bao bien
+//mutable: var
+// immutable: const
+var a = 10 
+const b = 10
+a = "10"
+//b ="11"
+console.log(a)
+console.log(b)
+// ctrl +'
+
+// kieu du lieu
+// null
+var a =null
+console.log(a)
+//undefined
+// Th1: khai bao bien khong cap gia tri (chi ap dung cho var va let)
+var c
+console.log(c)
+// Object (tim hieu camelcase)
+// Json
+const teo ={
+    name: "nguyen van teo",
+    age:20
+}
+console.log(teo.name.a)
+    
---
+++ Desktop/Javascipt/buoi2.js
@@ -0,0 +1,45 @@
+//4 :Array
+//const arrNames = ["Teo","Ti","Tun","Tuan"]
+//console.log(arrNames[0]) // truy cap thong thuong
+//console.log(arrNames)
+//const teo ={
+   // name: "Nguyen Van Teo"
+//}
+//console.log(teo["name"])
+// 5 :Arithmetic Operators
+// Toan Tu, Toan hang
+/*var a= 5
+a = a +1
+a++
+++a
+a +=1 */
+/*var a= 5
+a = ++a
+console.log(a) */
+/*var a =5
+var b = 5
+var KQ = a++ - --b +a++ - b++ + --a - b-- +a--
+// 5 - --b +a++ -b++ + --a - b-- + a--
+//5 - 4 +a++ - b++ + --a - b-- +a--
+//5 - 4 + 5 - b++ + --a -b-- +a--
+// 5 - 4 + 5 - 5 + --a - b-- + a--
+// 5 - 4 + 5 - 5 + 4 -b-- +a--
+// 5 -4 +5 -5 + 4 -5 +a--
+// 5 - 4 + 5 -5 + 4 - 5 +5
+console.log(KQ)*/
+// 6: Function
+/*function doSth (action){
+    console.log("thuc hien: " + action)
+
+}
+//doSth (" Chay bo ")
+console.log(doSth("chay bo"))*/
+// 7 : Object method
+const Teo = {
+    name: " Nguyen Van Teo",
+    age:10,
+    info: function (){
+        console.log("ten: " + this.name +". Tuoi" + this.age);
+    }
+}
+Teo.info()
