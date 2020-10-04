1719f3b82c8bfbbd5949157c820b69e4768559bf
Duongvanquang <15114129@st.hcmuaf.edu.vn>
2020-10-04 16:56:42 +0700

    bai tap

--- Desktop/bai tap Javascrip/baitap1.js
+++ Desktop/bai tap Javascrip/baitap1.js
@@ -3,7 +3,13 @@
     age: 23
 }
 console.log(Teo)*/
-var Width =10
+/*var Width =10
 var height =20 
 var s = Width * height
-console.log(s)
+console.log(s)*/
+/*var r = 10
+var s = r*r*3.14
+console.log(s)*/
+/*var a = 10
+var x = --a + a++
+console.log(x)*/
