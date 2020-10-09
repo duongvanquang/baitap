b0bbee32e6ea886dd613a868a4fedb92e387c20e
Duongvanquang <15114129@st.hcmuaf.edu.vn>
2020-10-09 16:06:12 +0700

    sua bai tap 3

--- Desktop/Javascipt/buoi2.js
+++ Desktop/Javascipt/buoi2.js
@@ -1,48 +1,50 @@
 //4 :Array
-//const arrNames = ["Teo","Ti","Tun","Tuan"]
-//console.log(arrNames[0]) // truy cap thong thuong
+/*const arrNames = ["Teo","Ti","Tun","Tuan"]
+console.log(arrNames[0]) // truy cap thong thuong*/
+
 //console.log(arrNames)
-//const teo ={
-   // name: "Nguyen Van Teo"
-//}
-//console.log(teo["name"])
+/*const teo ={
+   name: "Nguyen Van Teo"
+}
+console.log(teo["name"])*/
 // 5 :Arithmetic Operators
 // Toan Tu, Toan hang
 /*var a= 5
 a = a +1
 a++
 ++a
-a +=1 */
-/*var a= 5
+a +=1 
+var a= 5
 a = ++a
 console.log(a) */
+
 /*var a =5
 var b = 5
 var KQ = a++ - --b +a++ - b++ + --a - b-- +a--
-// 5 - --b +a++ -b++ + --a - b-- + a--
-//5 - 4 +a++ - b++ + --a - b-- +a--
-//5 - 4 + 5 - b++ + --a -b-- +a--
-// 5 - 4 + 5 - 5 + --a - b-- + a--
-// 5 - 4 + 5 - 5 + 4 -b-- +a--
-// 5 -4 +5 -5 + 4 -5 +a--
-// 5 - 4 + 5 -5 + 4 - 5 +5
+// 5 - --b +a++ -b++ + --a - b-- + a-- a = 6, b = 5
+//5 - 4 +a++ - b++ + --a - b-- +a-- a = 6, b = 4
+//5 - 4 + 6 - b++ + --a -b-- +a-- a = 7, b = 4
+// 5 - 4 + 6 - 4 + --a - b-- + a-- a = 7,b = 5
+// 5 - 4 + 6 - 4 + 6 -b-- +a-- a = 6 , b = 5
+// 5 -4 +6 -4 + 6 -5 +a-- a = 6, b = 4
+// 5 - 4 + 6 -4 + 6 - 5 + 6
 console.log(KQ)*/
 // 6: Function
 /*function doSth (action){
     console.log("thuc hien: " + action)
 
 }
-//doSth (" Chay bo ")
-console.log(doSth("chay bo"))*/
+doSth (" Chay bo ")*/
+//console.log(doSth("chay bo"))
 // 7 : Object method
  /*const Teo = {
     name: " Nguyen Van Teo",
     age:10,
     info: function (){
-        console.log("ten: " + this.name +". Tuoi" + this.age);
+        console.log("ten: " + this.name +". Tuoi: " + this.age);
     }
 }
-Teo.info()*/
+Teo.info() */
 // 8: phep so sanh
  /*var a = "5"
 var b =  "10"
@@ -53,14 +55,14 @@ console.log(Teo == Ti)*/
 //console.log(undefined +1)
 /*var a = 5
 var b = "5"
-console.log( a ===b)*/
-//console.log (typeof null)
+console.log( a ===b)
+console.log (typeof null)*/
 // 9: cau dieu kien
-var a =5
+/*var a =5
 var b = 10
 // 9 gia tri tuong trung cho false: 0, false, undefined, null,NaN, "", ! cho gia tri khac khong
 if(a>b){
     console.log("a lon hon b")
 } else {
     console.log("a nho hon hoac bang b")
-}
+}*/
--- Desktop/Javascipt/buoi3.js
+++ Desktop/Javascipt/buoi3.js
@@ -1,15 +1,11 @@
 // 10: Vong lap
-//const arrNumbers1 = [ 1,2,3,4,5]
-//const arrNumber2 =[10, 20,30]
-
-//for (var i =0;i <10 ;i++){
-//   console.log(i)
-
-//for (var i =0; i<arrNumbers1.length;i++){
-//   for(var y =0;y<arrNumber2.length;y++){
-//      console.log(arrNumbers1[i] * arrNumber2[y])
-//   }
-//}
+/*const arrNumbers1 = [ 1, 2, 3, 4, 5]
+const arrNumbers2 = [ 10, 20 ,30]
+for (var i =0; i < arrNumbers1.length; i++){
+    for ( var y = 0; y < arrNumbers2.length;y++){
+        console.log(arrNumbers1[i] * arrNumbers2[y])
+    }
+}*/
 // 10
 // 20
 // 30
@@ -26,101 +22,120 @@
 // 100
 // 150
 // 11: For of (lay gia tri trong mang)
-//const arrNames = ["Teo", "Ti","Tun","Hoa"]
-//for (const value of arrNames){
-//   console.log(value)
-//}
-/*function multiply (arr) {
-    var result =1
+/*const arrNames =  ["Teo","Ti","Hoa","Tun"]
+for (const value of arrNames){
+    console.log(value)
+} */
+/*function multiply (arr){
+    var result = 1 
     for (const value of arr){
         result *= value
     }
-    return result
+    return result 
 }
 console.log(multiply([2,3,4]));*/
-// for in (lay key trong mang)
-// const arrNames = ["Teo","Ti","Tun","Hoa"]
-/*const teo = {
-    name : "Nguyen Van Teo",
+// 12: for in (lay key trong mang)
+/*const arrNames = ["Teo","Ti","Tun","Hoa"]
+const teo ={
+    name: "Nguyen Van Teo",
     age: 20
 }
-for ( var key in teo){
-    console.log(key)
-}*/
-/*var apartment = {
-    bedroom: {
-        age: 20,
-        bed: {
-            type: 'twin - bed',
-            price: 100
+for (var key in teo){
+    console.log(key) */
+   /* var apartment ={
+        bedroom: {
+            area: 20,
+            bed : {
+                type:'twin - bed',
+                price:100
+            }
         }
-    }
-};*/
-/*function getkey(object){
-    for (const keyobject in object){
-        console.log(keyobject)
-        for (const keyBedroom in object [keyobject]){
-            console.log(keyBedroom)
-            for (const KeyBed in object [keyobject][keyBedroom]){
-                console.log(KeyBed)
+    };
+    function getKey(obj){
+        for (const key in obj){
+            console.log(key)
+            if(typeof obj [key]==="object"){
+                getKey(obj[key])
             }
         }
     }
-}*/
-/*function getkey (object){
-    for (const key in object){
-        console.log(key)
-        if(typeof object [key] === "object"){
-            getkey(object[key])
-        }
-    }
-}
-getkey(apartment) */
-//13: Arry method
-//const arrNums = [5,1,10,20,30]
-// Them phan tu cuoi mang
-/*arrNums.push (40)
-console.log(arrNums)*/
-// xoa phan tu cuoi
-//arrNums.pop()
-// them phan tu dau mang
-// arrNums.unshift (0)
-// xoa phan tu dau mang
+    getKey (apartment) */
+   // 13 : Array method
+// const arrNums = [5,1,10,20,30]
+// Them phần tử vào cuối mảng
+// arrNums.push(40)
+// Xóa phần tử ở cuối
+// arrNums.pop()
+// Thêm phần tử đầu mảng
+// arrNums.unshift(0)
+// Xóa phần tử đầu mảng
 // arrNums.shift()
-// them phan tu o giua
-// arrNums.splice(1,0,50)
-// xoa phan tu o giua
-// arrNums.splice (1,3)
-//14: callback la tham so
-/*function TingTong (a,b){
+// Xóa phần tử ở giữa
+// arrNums.splice(1 , 3)
+// Thêm phần tử ở giữa
+// arrNums.splice(-1 , 0 , 50)
+
+// console.log(arrNums)
+
+// 14: callback la tham so 
+/*function tinhtong(a ,b){
     return a + b
 }
-function inThongTin (cb){
+function inThongTin(cb){
     console.log(cb)
 }
-inThongTin(TingTong(10 ,5))*/
+inThongTin(tinhtong(10 ,5)) */
+// vong lap 1...100
+// in so chan
+// in so le
+// in so chia cho 3 du 1
+// in so chinh phuong
+
 /*function insochan (){
-    for (var i =1; i<=100;i++){
-        if (i %2 ==0){
+    for (var i = 1 ; i <= 100;i++){
+        if (i % 2=== 0 ){
             console.log(i)
         }
     }
 }
-function inso (sochia,sodu){
-    for (var i =0;i<=100;i++){
-        if(i % sochia ===sodu){
+insochan (1,100)*/
+/*function insole(){
+    for ( var i =1; i<=100;i++){
+        if(i % 2 ===1){
             console.log(i)
         }
     }
 }
-inso (1)*/
-function inso (cb){
-    for ( var i = 1 ; i<= 100;i++){
+insole (1,100)*/
+
+/*function insochia3du1 (){
+    for (var i = 1;i<=100;i++)
+    {
+        if(i % 3 ===1){
+            console.log(i)
+        }
+    }
+}
+insochia3du1 (1,100)*/
+
+/*function insochinhphuong (){
+    for ( var i =1;i <=100;i++){
+        if(Math.sqrt(i) % 1 ===0){
+            console.log(i)
+        }
+    }
+}
+insochinhphuong (1,100)*/
+function inSo(cb){
+    for (var i =1;i <= 100;i++){
         const dk = cb(i)
         if (dk) console.log(i)
     }
 }
-inso (function(i){
-    return Math.sqrt(i) % 1==0
+inSo (function(i){
+    return Math.sqrt(i) % 1 == 0
 })
-
+insochan (1,100)
+insole(1,100)
+insochia3du1 ()
+insochinhphuong ()
