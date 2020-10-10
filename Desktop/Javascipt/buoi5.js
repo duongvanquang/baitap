6d25d4007606f955dbe44d71a94c6bf93a4231fc
Duongvanquang <15114129@st.hcmuaf.edu.vn>
2020-10-10 18:17:32 +0700

    function scope

--- Desktop/Javascipt/buoi5.js
+++ Desktop/Javascipt/buoi5.js
@@ -1,5 +1,5 @@
 // 19 : Prototype
-function Person(name, age){
+/*function Person(name, age){
     this.name = name;
     this.age = age;
     this.setName = function(name){
@@ -9,3 +9,18 @@ function Person(name, age){
 const teo = new Person ("Nguyen van Teo", 20)
 teo.setName("Teo")
 console.log(teo)
+Person.prototype.getInFo = function (){
+    return this
+}
+const teo = new Person("Nguyen Van Teo",20);
+const ti = new Person ("Nguyen Van Ti",20);
+console.log(Person.prototype)*/
+// 20 : Function scope
+var a = 1
+function random (){
+    var a = Math.random ()
+    console.log(a)
+}
+random ()
+console.log(a)
+
