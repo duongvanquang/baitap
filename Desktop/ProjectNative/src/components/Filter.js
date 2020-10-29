b3bb5a5ca54347917ab1015c5db8a44b6cb4b31e
Duongvanquang <15114129@st.hcmuaf.edu.vn>
2020-10-29 12:39:48 +0700

    filterMode

---
+++ Desktop/ProjectNative/src/components/Filter.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+
+export default class Filter extends Component {
+  state = {
+    filterSelected: null,
+  };
+  render() {
+    return (
+      <View style={styles.containerPickerStyle}>
+        <RNPickerSelect
+          value={this.props.filterMode}
+          onValueChange={value => {}}
+          items={[
+            {label: 'Show All', value: 'Show_All'},
+            {label: 'Show Forgot', value: 'Show_Forgot'},
+            {label: 'Show Memorized', value: 'Show_Memorized'},
+          ]}
+        />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  containerPickerStyle: {
+    borderWidth: 1,
+    borderRadius: 1,
+    borderColor: 'black',
+    padding: 20,
+    marginHorizontal: 10,
+  },
+  pickerStyle: {
+    padding: 50,
+  },
+});
--- Desktop/ProjectNative/src/screens/MainScreen.js
+++ Desktop/ProjectNative/src/screens/MainScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import AppDimensions from '../untils/AppDimensions';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default class MainScreen extends Component {
     constructor(props) {
@@ -16,28 +17,48 @@ export default class MainScreen extends Component {
             txtEn: '',
             txtVn: '',
             shouldShowForm: false,
+            filterMode:null,
         }
     }
     toggleWord = word => {
-        const newWord = this.state.words.map(item => {
+        const newWords = this.state.words.map(item => {
             if (item.id === word.id) {
                 return { ...item, isMemorized: !item.isMemorized };
             }
             return item;
         });
-        this.setState({ words: newWord });
+        this.setState({ words: newWords });
     }
     RemoveWord = word => {
-        const newWord = this.state.words.filter(item => {
+        const newWords = this.state.words.filter(item => {
             if (item.id === word.id) {
                 return false;
             }
             return true;
         })
-        this.setState({ words: newWord });
+        this.setState({ words: newWords });
     }
     toggleForm = () => {
         this.setState({ shouldShowForm: !this.state.shouldShowForm })
+    };
+    addWord = () => {
+        {
+            const { txtEn, txtVn, words } = this.state;
+            const newWords = Object.assign([], words);
+            if (txtEn.length <= 0 || txtVn.length <= 0) {
+                return alert(' bạn chưa nhập đủ thông tin');
+            }
+            const newWord = {
+                id: words.length + 1,
+                en: txtEn,
+                vn: txtVn,
+                isMemorized: false,
+            };
+            newWords.unshift(newWord);
+            this.setState({ words: newWords, txtEn: '', txtVn: '' });
+            this.textInputEn.clear();
+            this.textInputVn.clear();
+        }
     }
     renderForm = shouldShowForm => {
         if (shouldShowForm) {
@@ -45,16 +66,16 @@ export default class MainScreen extends Component {
                 <View>
                     <View style={styles.containerTextInput}>
                         <TextInput
-                            oonChangeText={text => (this.state.textEn = text)}
+                            onChangeText={text => (this.state.txtEn = text)}
                             ref={refs => (this.textInputEn = refs)}
                             placeholder="English"
                             style={styles.textInput} />
-                        <TextInput onChangeText={text => (this.state.textVn = text)}
+                        <TextInput onChangeText={text => (this.state.txtVn = text)}
                             ref={refs => (this.textInputVn = refs)}
                             placeholder="Vietnamese"
                             style={styles.textInput} />
                     </View>
-                    <View style={styles.containerTouchable}>
+                    <View style={styles.containerTouchableForm}>
                         <TouchableOpacity
                             onPress={this.addWord}
                             style={styles.touchableAddword}>
@@ -94,11 +115,11 @@ export default class MainScreen extends Component {
                         onPress={() => this.toggleWord(word)}
                         style={{
                             ...styles.touchableMemorized,
-                            backgroundColor: word.isMemorized ? '#DD3444' : '#28a845'
+                            backgroundColor: word.isMemorized ? '#28a845' : '#DD3444'
                         }}
                         activeOpacity={0.1}>
                         <Text style={styles.textSize}>
-                            {word.isMemorized ? 'Memorized' : 'Forgot'} </Text>
+                            {word.isMemorized ? 'Forgot' : 'Memorized'} </Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={() => this.RemoveWord(word)}
@@ -110,20 +131,47 @@ export default class MainScreen extends Component {
             </View>
         );
     }
+    rendenFilter = () => {
+        return (
+            <View style={styles.containerPickerStyle}>
+                <RNPickerSelect
+                onDonePress={()=>{
+                    const {words,filterMode}= this.state
+                    const newWords = words.filter(word =>{
+                        if(this.state.filterMode === 'Show_All'){
+                            return true;
+                        } else if (filterMode ==='Show_Forgot' && word.isMemorized){
+                            return true;
+                        } else if(filterMode ==='Show_Memoriz' && !word.isMemorized){
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                    this.setState({words:newWords})
+                }}
+                    onValueChange={value => {
+                        this.state.filterMode = value
+                     }}
+                    items={[
+                        { label: 'Show All', value: 'Show_All' },
+                        { label: 'Show Forgot', value: 'Show_Forgot' },
+                        { label: 'Show Memorized', value: 'Show_Memorized' },
+                    ]}
+                />
+            </View>
+        );
+    }
     render() {
         return (
             <View style={styles.container}>
                 {this.renderForm(this.state.shouldShowForm)}
+                {this.rendenFilter()}
                 {this.state.words.map(word => this.renderItemWord(word))}
             </View>
         );
     }
 }
-
-
-
-
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -213,5 +261,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-evenly',
         marginBottom: 10,
+    },
+    containerPickerStyle: {
+        borderWidth: 1,
+        borderRadius: 1,
+        borderColor: 'black',
+        padding: 20,
+        marginHorizontal: 10,
+    },
+    pickerStyle: {
+        padding: 50,
     },
 });
